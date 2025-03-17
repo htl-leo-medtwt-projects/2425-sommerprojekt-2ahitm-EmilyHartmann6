9b64db500c6921e1d10fbbc410b8e03e6066a720
EmilyHartmann6 <e.hartmann@students.htl-leonding.ac.at>
2025-03-17 10:52:33 +0100

    progress

--- project/Where are you/script.js
+++ project/Where are you/script.js
@@ -1,1 +1,28 @@
+let optionOutput = document.getElementById("optionOutput");
+let optionButton = document.getElementById("optionButton");
+function options(){
+    optionOutput.innerHTML = `
+    <h2 class="middelHeading">Volume</h2>
+    <div class="volume-container">
+        <div class="bar" data-level="1"></div>
+        <div class="bar" data-level="2"></div>
+        <div class="bar" data-level="3"></div>
+        <div class="bar" data-level="4"></div>
+        <div class="bar" data-level="5"></div>
+    </div>`;
+    optionButton.style.color = "#702020"
+}
+const bars = document.querySelectorAll(".bar");
 
+bars.forEach(bar => {
+    bar.addEventListener("click", function() {
+        let volumeLevel = parseInt(this.getAttribute("data-level"));
+        bars.forEach((b, index) => {
+            if (index < volumeLevel) {
+                b.classList.add("active");
+            } else {
+                b.classList.remove("active");
+            }
+        });
+    });
+});
